56b925bfc9e09161feaeb2ab273c2647572c9064
Caroline Wilson <carolinecwilson3@gmail.com>
2017-10-05 21:27:00 +0100

    Reduce bottle count and use ocean image

--- _site/js/countup.js
+++ _site/js/countup.js
@@ -1,7 +1,7 @@
 var total = 0;
 var countupfunction = setInterval(function() {
 
-    var bottlesPerSecond = 11690;
+    var bottlesPerSecond = 6341;
     total = total + bottlesPerSecond;
     document.getElementById("totalBottles").innerHTML = total.toLocaleString('en');
 }, 1000);
