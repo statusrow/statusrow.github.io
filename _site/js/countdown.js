0e1ced8196a34d9a40364013fb72d1f143657c5a
Caroline Wilson <carolinecwilson3@gmail.com>
2019-03-03 18:21:48 +0000

    Update images, contact and homepage copy post-row

--- _site/js/countdown.js
+++ _site/js/countdown.js
@@ -1,43 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-  var startDate = new Date("Dec 12, 2018 10:00:00").getTime();
-
-  var countdownfunction = setInterval(function() {
-
-      var now = new Date().getTime();
-
-      var distance = startDate - now;
-
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      var daysElem = document.getElementById("countdown-days");
-      var hoursElem = document.getElementById("countdown-hrs");
-      var minutesElem = document.getElementById("countdown-mins");
-      var secondsElem = document.getElementById("countdown-secs");
-
-      if (daysElem && hoursElem && minutesElem && secondsElem) {
-        daysElem.innerHTML = days;
-        hoursElem.innerHTML = hours;
-        minutesElem.innerHTML = minutes;
-        secondsElem.innerHTML = seconds;
-      }
-
-      if (distance < 0) {
-        clearInterval(countdownfunction);
-        daysElem.innerHTML = 0;
-        hoursElem.innerHTML = 0;
-        minutesElem.innerHTML = 0;
-        secondsElem.innerHTML = 0;
-      }
-  }, 1000);
-
   function setBackgroundImage() {
     var images = [
-      "url(/assets/images/Talisker_Whisky_Atlantic_Challenge_Status_Row_Plastic_Pollution_1.jpg)",
-      "url(/assets/images/Talisker_Whisky_Atlantic_Challenge_Status_Row_Plastic_Pollution_2.jpg)",
-      "url(/assets/images/Talisker_Whisky_Atlantic_Challenge_Status_Row_Plastic_Pollution_3.jpg)"
+      "linear-gradient(to right top, rgba(0, 51, 119, 0.8), rgba(100, 235, 255, 0.36)), url(/assets/images/TWAC_2018_Status_Row_1.jpg)",
+      "linear-gradient(to right top, rgba(0, 51, 119, 0.8), rgba(100, 235, 255, 0.36)), url(/assets/images/TWAC_2018_Status_Row_2.jpg)",
+      "linear-gradient(to right top, rgba(0, 51, 119, 0.8), rgba(100, 235, 255, 0.36)), url(/assets/images/TWAC_2018_Status_Row_3.jpg)",
+      "linear-gradient(to right top, rgba(0, 51, 119, 0.8), rgba(100, 235, 255, 0.36)), url(/assets/images/TWAC_2018_Status_Row_4.jpg)"
     ];
 
     var x = document.getElementsByClassName("Hero__Global");
